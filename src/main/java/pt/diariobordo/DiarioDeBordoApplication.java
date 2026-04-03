587c28dcc0f1adb7bf2eecd923b2e8d42604425b
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2026-04-03 18:45:56 +0100

    estrutura inicial

---
+++ src/main/java/pt/diariobordo/DiarioDeBordoApplication.java
@@ -0,0 +1,13 @@
+package pt.diariobordo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioDeBordoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioDeBordoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/diariobordo/DiarioDeBordoApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.diariobordo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioDeBordoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
